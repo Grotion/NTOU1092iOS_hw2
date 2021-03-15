c1b0147ec38b9c4e49f5b6f7de165f698485e8af
GroTion <grotion0720@gmail.com>
2021-03-16 01:01:09 +0800

    Initial Commit

---
+++ ntou1092iOS_hw2/ntou1092iOS_hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  ntou1092iOS_hw2App.swift
+//  ntou1092iOS_hw2
+//
+//  Created by Shaun Ku on 2021/3/16.
+//
+
+import SwiftUI
+
+@main
+struct ntou1092iOS_hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
